9e7fec90902422e7226f92bcf981017bd6734088
pranatalite12 <pranata.giya12.gp@gmail.com>
2023-06-04 10:43:26 +0700

    init

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// read message from client
+		_, message, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		// show message
+		log.Printf("Received message: %s", message)
+
+		//send message to client
+		err = conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+	}
+}
+
+func main() {
+	http.HandleFunc("/websocket", websocketHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
